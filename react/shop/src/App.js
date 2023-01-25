329dc7ed93e1dd3b86644f5da45984005f94a699
onezeun <onezeun@gmail.com>
2023-01-25 22:37:25 +0900

    Part2 1~4

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,20 +1,48 @@
-import { Navbar, Container, Nav} from 'react-bootstrap'
+import { useState } from 'react';
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
+import './App.css'
+import bg from './bg.png'
+import shoesData from './data';
+
 
 function App() {
+  let [shoes, setShoes] = useState(shoesData);
+
   return (
     <div className="App">
-            <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
+
+      <div className='main-bg' style={{ backgroundImage: `url(${bg})` }}></div>
+
+      <Container>
+        <Row className="justify-content-center">
+          <Card shoes={shoes}></Card>
+        </Row>
+      </Container>
     </div>
   );
+}
+
+function Card({ shoes }) {
+  return (
+    shoes.map(function (data, i) {
+      return (
+        <Col xs={12} md={4}>
+          <img src={`https://codingapple1.github.io/shop/shoes${i+1}.jpg`} width="80%" alt="" />
+          <h4>{data.title}</h4>
+          <p>{data.price}</p>
+        </Col>
+      )
+    })
+  )
 }
 
 export default App;
