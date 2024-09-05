193b93f6503a721d92b07b76fb1864a9ac8ce6fb
onezeun <onezeun@gmail.com>
2024-09-05 14:32:51 +0900

    immer

--- next/zustand/app/_store/LetterStore.ts
+++ next/zustand/app/_store/LetterStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { immer } from 'zustand/middleware/immer';
 
 interface LetterState {
   id: number;
@@ -15,22 +16,21 @@ interface LetterListState {
 
 export const useLetterStore = create<LetterListState>()(
   devtools(
-    (set) => ({
+    immer((set) => ({
       letters: [],
       addLetter: ({ title, content }) =>
-        set((state) => ({
-          letters: [
-            ...state.letters,
-            {
-              id: state.letters.length + 1,
-              title,
-              content,
-            },
-          ],
-        })),
+        set((state) => {
+          // 새로운 편지를 직접 추가하는 것처럼 작성
+          // 원래 상태를 직접 수정하는 것처럼 보이지만, immer가 내부적으로 알아서 복사본을 만들어서 상태를 안전하게 변경
+          state.letters.push({
+            id: state.letters.length + 1,
+            title,
+            content,
+          });
+        }),
       removeLetter: (id: number) =>
-        set((prev) => ({ letters: prev.letters.filter((e) => e.id !== id) })),
-    }),
+        set((state) => ({ letters: state.letters.filter((e) => e.id !== id) })),
+    })),
     { name: 'LetterStore' } // DevTools에서 스토어 이름을 지정
   )
 );
