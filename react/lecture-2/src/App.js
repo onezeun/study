e9cf70a8956df7a6fd13ee6481d7734361941269
onezeun <onezeun@gmail.com>
2025-11-07 16:38:08 +0900

    컴포넌트 Preloading

--- react/lecture-2/src/App.js
+++ react/lecture-2/src/App.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, Suspense, lazy } from 'react';
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
 import SurveyChart from './components/SurveyChart'
 import Footer from './components/Footer'
-import ImageModal from './components/ImageModal'
+import { Component } from 'react';
+
+
+function LazyWithPreload(importFunction) {
+    const component = React.lazy(importFunction);
+    component.preload = importFunction;
+    return component
+}
+
+const LazyImageModal = LazyWithPreload(() => import('./components/ImageModal'));
 
 function App() {
     const [showModal, setShowModal] = useState(false)
+
+    useEffect(() => {
+        LazyImageModal.preload()
+
+        const img = new Image()
+        img.src =
+          'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800';
+    },[])
+
+    // const handleMouseEnter = () => {
+    //     const component = import('./components/ImageModal');
+    // }
 
     return (
         <div className="App">
             <Header />
             <InfoTable />
-            <ButtonModal onClick={() => { setShowModal(true) }}>올림픽 사진 보기</ButtonModal>
+            <ButtonModal onClick={() => { setShowModal(true)}} /* onMouseEnter={handleMouseEnter} */ >올림픽 사진 보기</ButtonModal>
             <SurveyChart />
             <Footer />
-            {showModal ? <ImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            <Suspense fallback={null}>
+                {showModal ? <LazyImageModal closeModal={() => { setShowModal(false) }} /> : null}
+            </Suspense>
         </div>
     )
 }
